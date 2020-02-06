8a0d95b5b781c3253b2bc539061df950c85022a3
胡怡 <49391498+hy1997@users.noreply.github.com>
2020-02-06 21:31:32 +0800

    update

---
+++ src/main/java/com/huyi/demo/MybatisgenerateApplication.java
@@ -0,0 +1,17 @@
+package com.huyi.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+
+@SpringBootApplication
+public class MybatisgenerateApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisgenerateApplication.class, args);
+    }
+
+}
