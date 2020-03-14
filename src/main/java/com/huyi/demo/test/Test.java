d8ba3c24ad8aec611286451f8ea0da83f896b93d
胡怡 <49391498+hy1997@users.noreply.github.com>
2020-03-14 22:22:09 +0800

    update

--- src/main/java/com/huyi/demo/test/Test.java
+++ src/main/java/com/huyi/demo/test/Test.java
@@ -5,24 +5,6 @@ public class Test {
         System.out.println(2);
     }
 
-    public   String Demo(){
-        System.out.println(8);
-        return "!2";
-    }
-
-  public void   demo()
-  {
-      System.out.println("#23");
-  }
-    public void   testBranch()
-    {
-        System.out.println("#23");
-    }
-
-    public void   testB123ranch()
-    {
-        System.out.println("#23");
-    }
     public void   uat()
     {
         System.out.println("#23");
