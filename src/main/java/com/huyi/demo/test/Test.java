e6a2ded702de37123825c53019283bd0c74a121f
胡怡 <49391498+hy1997@users.noreply.github.com>
2020-03-14 22:03:33 +0800

    update

--- src/main/java/com/huyi/demo/test/Test.java
+++ src/main/java/com/huyi/demo/test/Test.java
@@ -2,6 +2,11 @@ package com.huyi.demo.test;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println(2);
+    }
+
+    public   String Demo(){
         System.out.println(1);
+        return "!2";
     }
 }
