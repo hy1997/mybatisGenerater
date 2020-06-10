7e550bb95e55054564bf1e02d66fa8a66661da02
hy1997 <1007885927@qq.com>
2020-06-10 12:31:09 +0800

    修改

--- src/main/java/com/huyi/demo/MybatisgenerateApplication.java
+++ src/main/java/com/huyi/demo/MybatisgenerateApplication.java
@@ -3,10 +3,11 @@ package com.huyi.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 2020 6 10
+ */
 @SpringBootApplication
 public class MybatisgenerateApplication {
-
-
 
     public static void main(String[] args) {
 
